e11a767ecacee353179439b0b6903228a666b9ef
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-03-23 10:53:54 +0200

    v2.5.0

--- History/me/index.js
+++ History/me/index.js
@@ -25,3 +25,27 @@ projectHistoryMainContainer.innerHTML += main.innerHTML =
     ["Lorem ipsum dolor sit amet consectetur adipisicing elit. 1"],
     [`./Mar19th2023/Screenshot_1.png`, `./Mar19th2023/Screenshot_2.png`]
   );
+projectHistoryMainContainer.innerHTML += main.innerHTML =
+  projectHistoryComponent(
+    `PROJECTS`,
+    `097ac7593f514cab11c91b2b15b8d4dcd4476f33`,
+    `IIWeJHmh0xY`,
+    ["Lorem ipsum dolor sit amet consectetur adipisicing elit. 1"],
+    [`./Mar19th2023/Screenshot_1.png`, `./Mar19th2023/Screenshot_2.png`]
+  );
+projectHistoryMainContainer.innerHTML += main.innerHTML =
+  projectHistoryComponent(
+    `PROJECTS`,
+    `818ead2312dd04b7da26b16df60492d7b7de4038`,
+    `IIWeJHmh0xY`,
+    [
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. 1",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. 1",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. 1",
+    ],
+    [
+      `./Mar19th2023/Screenshot_1.png`,
+      `./Mar19th2023/Screenshot_2.png`,
+      `./Mar19th2023/Screenshot_8.png`,
+    ]
+  );
