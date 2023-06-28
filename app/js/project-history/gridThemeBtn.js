c19b68893a1e126ba6e2639304f9468fd6725abf
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-06-28 09:23:40 +0300

    v3.0.0
    Start All Over w/ React & TS & TailwindCSS
    this repo is contain Projects History (aka, "screenshots, YouTube preview, github commit data")

--- app/js/project-history/gridThemeBtn.js
+++
@@ -1,9 +0,0 @@
-document
-  .querySelector("#gridThemeBtn > button")
-  .addEventListener("click", () => {
-    if (document.querySelector("#main").classList.contains("grid-theme")) {
-      document.querySelector("#main").classList.remove("grid-theme");
-    } else {
-      document.querySelector("#main").classList.add("grid-theme");
-    }
-  });
--- app/js/script.js
+++
@@ -1,4 +0,0 @@
-console.log("first code 2023-3-15 3:20AM");
-
-const mainContaner = document.querySelector("#projectsContainer");
-const projectHistoryMainContainer = document.querySelector("#main");
